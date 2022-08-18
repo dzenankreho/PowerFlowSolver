598dbb7a21b9fcdf8eb6a0eb33e12651f8279d5e
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-08-18 13:06:10 +0200

    Update export.h

--- SREESseminarski/export.h
+++ SREESseminarski/export.h
@@ -2,9 +2,15 @@
 #include <fstream>
 #include <iostream>
 #include "systemModel.h"
+#include <string.h>
+
 
 void exportToLatex(SystemModel::SystemModel s);
 
 void exportToHTML(SystemModel::SystemModel s);
 
-void exportToTxt(const char* filename, SystemModel::SystemModel s);
+void exportToTxt(const char* filename, SystemModel::SystemModel s,
+                 std::vector<std::vector<std::tuple<int, double, double, std::string, double, double, int>>> buses,
+                 std::vector<std::tuple<int, int, double, double, double,std::string, double, double, double, double>> lineValues,
+                 std::vector<std::tuple<int, double, std::string, std::string, double, double>> batteryValues,
+                 std::vector<std::tuple<int, int, double, double, double, double,std::string, double, double>> transValues);
