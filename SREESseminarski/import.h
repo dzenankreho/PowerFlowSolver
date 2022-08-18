a1df93c13ddf5f1fcdeed0ead5be2c6c298df33e
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-08-18 13:07:02 +0200

    Update import.h

--- SREESseminarski/import.h
+++ SREESseminarski/import.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "systemModel.h"
+#include <string.h>
 
 
-void importFromTxt(const char* filename, SystemModel::SystemModel& s);
+int importFromTxt(const char* filename, SystemModel::SystemModel& s,
+                   std::vector<std::vector<std::tuple<int, double, double, std::string, double, double, int>>>* buses,
+                   std::vector<std::tuple<int, double, double, std::string, double, double, int>>* sortedBuses,
+                   std::vector<std::tuple<int, int, double, double, double, std::string, double, double, double, double>>* lineValues,
+                   std::vector<std::tuple<int, double, std::string, std::string, double, double>>* batteryValues,
+                   std::vector<std::tuple<int, int, double, double, double, double, std::string, double, double>>* transValues);
