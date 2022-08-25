929dbf4455cd65059d34cb0d78ebdf8e968bf1ff
dzeni21 <85407660+dzeni21@users.noreply.github.com>
2022-08-25 20:33:18 +0200

    Update import.cpp

--- SREESseminarski/import.cpp
+++ SREESseminarski/import.cpp
@@ -14,7 +14,7 @@
 /// <param name="const char*">Name of the file</param>
 /// <param name="SystemModel::SystemModel">System model</param>
 /// <returns></returns>
-int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
+int importFromTxt(const char* filename,
                    std::vector<std::vector<std::tuple<int, double, double, std::string, double, double, int>>>* buses,
                    std::vector<std::tuple<int, double, double, std::string, double, double, int>>* sortedBuses,
                    std::vector<std::tuple<int, int, double, double, double, std::string, double, double, double, double>>* lineValues,
@@ -52,12 +52,8 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                     inFile >> line >> color;
                     inFile >> line >> x;
                     inFile >> line >> y;
-                    systemModel.addBus(SystemModel::TypeOfBus::Slack);
-                    systemModel.getBus(busNum).setVoltageMagnitude(voltageMagnitude);
-                    systemModel.getBus(busNum).setVoltagePhase(voltagePhase);
                     slackValues.push_back(std::make_tuple(busNum, voltageMagnitude, voltagePhase, color, x, y, 1));
                     sortedBuses->push_back(slackValues[i]);
-//                    std::strcpy((std::string), color);
                     i++;
                     busNum++;
                 }
@@ -70,9 +66,6 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                     inFile >> line >> color;
                     inFile >> line >> x;
                     inFile >> line >> y;
-                    systemModel.addBus(SystemModel::TypeOfBus::PV);
-                    systemModel.getBus(busNum).setVoltageMagnitude(voltageMagnitude);
-                    systemModel.getBus(busNum).setActivePower(activePower);
                     pvValues.push_back(std::make_tuple(busNum, voltageMagnitude, activePower, color, x, y, 2));
                     sortedBuses->push_back(pvValues[j]);
                     j++;
@@ -88,9 +81,6 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                     inFile >> line >> color;
                     inFile >> line >> x;
                     inFile >> line >> y;
-                    systemModel.addBus(SystemModel::TypeOfBus::PQ);
-                    systemModel.getBus(busNum).setReactivePower(reactivePower);
-                    systemModel.getBus(busNum).setActivePower(activePower);
                     pqValues.push_back(std::make_tuple(busNum, activePower, reactivePower, color, x, y, 3));
                     sortedBuses->push_back(pqValues[k]);
                     k++;
@@ -130,7 +120,6 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                     inFile >> line >> x;
                     inFile >> line >> y;
 
-                    systemModel.addTransformer(bus1, bus2, seriesResistance, seriesReactance, shuntConductance, shuntSusceptance);
                     transValues->push_back(std::make_tuple(bus1, bus2, seriesResistance,seriesReactance,shuntConductance,shuntSusceptance, color, x, y));
 
                 }
@@ -149,7 +138,6 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                     inFile >> line >> x2;
                     inFile >> line >> y2;
 
-                    systemModel.addLine(bus1, bus2, seriesResistance, seriesReactance, shuntSusceptance);
                     lineValues->push_back(std::make_tuple(bus1, bus2, seriesResistance,seriesReactance,shuntSusceptance, color, x1, y1 ,x2, y2));
 
                 }
@@ -174,15 +162,6 @@ int importFromTxt(const char* filename, SystemModel::SystemModel& systemModel,
                 inFile >> line >> color;
                 inFile >> line >> x;
                 inFile >> line >> y;
-                if (configurationType == "Delta")
-                    systemModel.addCapacitorBank(busNum, capacitance, SystemModel::ThreePhaseLoadConfigurationsType::Delta);
-                else if (configurationType == "Grounded")
-                {
-                    systemModel.addCapacitorBank(busNum, capacitance, SystemModel::ThreePhaseLoadConfigurationsType::GroundedStar);
-                    configurationType = "Grounded Star";
-                }
-                else if (configurationType == "Star")
-                    systemModel.addCapacitorBank(busNum, capacitance, SystemModel::ThreePhaseLoadConfigurationsType::Star);
                 inFile >> line;
                 for (int x = 0; x < (configurationType.length() + 1); x++)
                         config[x] = configurationType[x];
